45ddfc1f07f64922e8ffc4789c4b0320738b1e5d
Djailla <bastien.vallet@gmail.com>
2016-09-12 15:20:21 +0200

    WIP, continue paperless integration

--- com.djailla.paperless/build.sh
+++ com.djailla.paperless/build.sh
@@ -5,11 +5,11 @@ mkdir -m 755 -p /opt/
 install -m 755 /home/source/rc.local /etc
 
 apt-get update
-apt-get install -y -q python3 python3-pip curl git libjpeg8 libjpeg8-dev
+apt-get install -y -q python3 python3-pip curl git libjpeg62 libjpeg62-turbo-dev zlib1g-dev
 rm /usr/bin/python
 ln -s /usr/bin/python3 /usr/bin/python
 rm /usr/bin/pip
-ln -s /usr/bin/pip-3.2 /usr/bin/pip
+ln -s /usr/bin/pip3 /usr/bin/pip
 
 # Checkout sources in /opt
 cd /opt/
